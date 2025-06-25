2736119502e7b9dbdf2217b385e43ccf23ca57f7
KhokharWelfare <khokharwelfare2024@gmail.com>
2025-06-25 17:22:13 +0500

    Update Donate.jsx

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -64,7 +64,7 @@ function Donate() {
     }
 
     try {
-      const response = await axios.post('https://khokharwelfare-backend-1.vercel.app/api/donation', data, {
+      const response = await axios.post('https://khokharwelfare-backend-1-eta.vercel.app/api/donation', data, {
         headers: {
           Authorization: `Bearer ${token}`,
           'Content-Type': 'multipart/form-data',
