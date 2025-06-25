64aa82e8ebc226c266e179fc03ed9587fd02f6c8
KhokharWelfare <khokharwelfare2024@gmail.com>
2025-06-25 22:55:03 +0500

    Update ViewUsage.jsx

--- src/pages/ViewUsage.jsx
+++ src/pages/ViewUsage.jsx
@@ -8,7 +8,7 @@ function ViewUsage() {
   const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
-    axios.get('https://khokharwelfare-backend.vercel.app/api/transaction')
+    axios.get('khokharwelfare-backend-1-production.up.railway.app/api/transaction')
       .then(res => setTransactions(res.data.transactions))
       .catch(err => console.error(err));
   }, []);
